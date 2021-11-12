a451f0ad681dc35fe75f338ec9749aaa3f6cb4f0
Guilherme Delarmelindo <gdelarme@c1r13s1.42lisboa.com>
2021-11-12 18:07:53 +0000

    added ft_memmove

---
+++ ft_memmove.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdelarme <gdelarme@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/10 21:43:51 by gdelarme          #+#    #+#             */
+/*   Updated: 2021/11/12 18:06:32 by gdelarme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+	size_t	n;
+
+	i = 0;
+	n = len - 1;
+	if (!dst && !src)
+		return (0);
+	if (dst > src)
+	{
+		while (i < len)
+		{
+			((char *)dst)[n - i] = ((char *)src)[n - i];
+			i++;
+		}
+	}
+	else
+	{
+		while (i < len)
+		{	
+			((char *)dst)[i] = ((char *)src)[i];
+			i++;
+		}
+	}
+	return (dst);
+}
