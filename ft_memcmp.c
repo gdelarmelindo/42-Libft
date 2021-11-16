962ea484641e5bbeca1498bbfa969d2bc0b9e49a
Guilherme Delarmelindo <gdelarme@c1r22s7.42lisboa.com>
2021-11-16 20:56:20 +0000

    added ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdelarme <gdelarme@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 20:25:52 by gdelarme          #+#    #+#             */
+/*   Updated: 2021/11/16 20:55:28 by gdelarme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return ((int)(((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]));
+		i++;
+	}
+	return (0);
+}
