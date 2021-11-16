5db10c2eaa82dce11482316fb761c44ba046f5d1
Guilherme Delarmelindo <gdelarme@c1r22s7.42lisboa.com>
2021-11-16 22:09:56 +0000

    added ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdelarme <gdelarme@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 20:57:36 by gdelarme          #+#    #+#             */
+/*   Updated: 2021/11/16 22:08:54 by gdelarme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (!needle)
+		return (&((char *)haystack)[i]);
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j])
+		{
+			if (needle[j + 1] == '\0')
+				return (&((char *)haystack)[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
