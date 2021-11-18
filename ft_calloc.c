3e9f68f98e8f90366c5651443953162f2c131d5b
Guilherme Delarmelindo <gdelarme@c1r19s9.42lisboa.com>
2021-11-18 18:55:52 +0000

    added ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdelarme <gdelarme@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/18 18:32:39 by gdelarme          #+#    #+#             */
+/*   Updated: 2021/11/18 18:54:50 by gdelarme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+	size_t	i;
+
+	i = 0;
+	ptr = malloc(count * size);
+	if (ptr)
+	{
+		while (i < size)
+		{
+			((char *)ptr)[i] = '0';
+			i++;
+		}
+	}
+	return (ptr);
+}
