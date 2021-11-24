8ddffe6f175b3133852eaed1a202623bec91619b
Guilherme Delarmelindo <gdelarme@c1r20s7.42lisboa.com>
2021-11-24 19:58:26 +0000

    updated Makefile and libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gdelarme <gdelarme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 22:13:22 by gdelarme          #+#    #+#             */
-/*   Updated: 2021/11/23 22:50:13 by gdelarme         ###   ########.fr       */
+/*   Updated: 2021/11/24 16:22:16 by gdelarme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
