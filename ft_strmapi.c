0c71757fb9c01529962a96f8ca0a8ba02bce1e1b
Guilherme Delarmelindo <gdelarme@c1r20s7.42lisboa.com>
2021-11-26 19:02:22 +0000

    added ft_strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdelarme <gdelarme@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 18:19:41 by gdelarme          #+#    #+#             */
+/*   Updated: 2021/11/26 19:00:12 by gdelarme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	int		i;
+
+	if (!s || !f)
+		return (0);
+	str = malloc(sizeof(*str) * (ft_strlen(s) + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
